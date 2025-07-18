ae847b28d50c6f2c8642ef715d2889846222f5bb
cthulhu <>
2025-07-18 02:45:29 +0200

    Ref: Move /resources/* -> /src/res/*

--- src/settings.py
+++ src/settings.py
@@ -2,9 +2,9 @@
 import os
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-FONT_PATH = os.path.abspath(BASE_PATH + '/../resources/font') + '/'
-IMG_PATH = os.path.abspath(BASE_PATH + '/../resources/img') + '/'
-SND_PATH = os.path.abspath(BASE_PATH + '/../resources/snd') + '/'
+FONT_PATH = os.path.abspath(BASE_PATH + '/res/font') + '/'
+IMG_PATH = os.path.abspath(BASE_PATH + '/res/img') + '/'
+SND_PATH = os.path.abspath(BASE_PATH + '/res/snd') + '/'
 
 SCALE = 3
 SCREEN_SIZE = (320 * SCALE, 200 * SCALE)
