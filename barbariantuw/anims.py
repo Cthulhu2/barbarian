Cthulhu2/barbarian

commit 396bb036dc77d5a170cbc310683aefac1828c769

Ref: Animations w snd action

barbariantuw/anims.py
@@ -97,6 +97,8 @@ class Frame(object):
 
 
 class Actions:
+    snd_acts = {}
+
     @staticmethod
     def kill(sprite):
         sprite.kill()
@@ -104,6 +106,17 @@ class Actions:
     @staticmethod
     def stop(sprite):
         sprite.stopped = True
+
+    @staticmethod
+    def snd(snd: str):
+        if snd in Actions.snd_acts:
+            return Actions.snd_acts[snd]
+
+        def act(sprite):
+            sprite.snd_play(snd)
+
+        Actions.snd_acts[snd] = act
+        return act
 
 
 def rtl_anims(anims: Dict[str, List[Frame]]):
@@ -363,6 +376,7 @@ def barb(subdir: str):
             Frame(f'{subdir}/debout.gif'),
         ],
         'attente': [
+            Frame(f'{subdir}/attente1.gif', tick=8, post_action=Actions.snd('attente.ogg')),
             Frame(f'{subdir}/attente1.gif', tick=15),
             Frame(f'{subdir}/attente2.gif', tick=23),
             Frame(f'{subdir}/attente3.gif', tick=30),
@@ -404,7 +418,8 @@ def barb(subdir: str):
         ],
         'rouladeAV': [
             # @formatter:off
-            Frame(f'{subdir}/roulade1.gif',             tick=4,  mv=(CHAR_W, 0)),  # noqa
+            Frame(f'{subdir}/roulade1.gif',             tick=2,  mv=(CHAR_W, 0), post_action=Actions.snd('roule.ogg')),  # noqa
+            Frame(f'{subdir}/roulade1.gif',             tick=4,                ),  # noqa
             Frame(f'{subdir}/roulade1.gif',             tick=7,  mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade1.gif',             tick=10, mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade2.gif',             tick=13, mv=(CHAR_W, 0)),  # noqa
@@ -433,7 +448,7 @@ def barb(subdir: str):
         ],
         'rouladeAR': [
             # @formatter:off
-            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=2,                 ),  # noqa
+            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=2,  post_action=Actions.snd('roule.ogg')),  # noqa
             Frame(f'{subdir}/roulade1.gif', xflip=True, tick=5,  mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade5.gif',             tick=8,  mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade2.gif', xflip=True, tick=11, mv=(-CHAR_W, 0)),  # noqa
@@ -448,7 +463,8 @@ def barb(subdir: str):
         ],
         'protegeH': [
             # @formatter:off
-            Frame(f'{subdir}/marche1.gif',  tick=5, mv=(-CHAR_W, 0)),
+            Frame(f'{subdir}/marche1.gif',  tick=2, mv=(-CHAR_W, 0), post_action=Actions.snd('protege.ogg')),
+            Frame(f'{subdir}/marche1.gif',  tick=5),
             Frame(f'{subdir}/protegeH.gif', tick=9, dx=-2 * SCALE_X),
             # @formatter:on
         ],
@@ -458,14 +474,14 @@ def barb(subdir: str):
         ],
         'cou': [
             # @formatter:off
-            Frame(f'{subdir}/protegeH.gif', tick=15, dx=-2 * SCALE_X),
+            Frame(f'{subdir}/protegeH.gif', tick=15, dx=-2 * SCALE_X, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/cou2.gif',     tick=30, dx=-4 * SCALE_X),
             Frame(f'{subdir}/cou3.gif',     tick=46, dx=-1 * SCALE_X),
             # @formatter:on
         ],
         'devant': [
             # @formatter:off
-            Frame(f'{subdir}/devant1.gif', tick=10),
+            Frame(f'{subdir}/devant1.gif', tick=10, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/devant2.gif', tick=20),
             Frame(f'{subdir}/devant3.gif', tick=30),
             Frame(f'{subdir}/devant2.gif', tick=46),
@@ -473,7 +489,7 @@ def barb(subdir: str):
         ],
         'genou': [
             # @formatter:off
-            Frame(f'{subdir}/genou1.gif', tick=10, dx=CHAR_W / 4),
+            Frame(f'{subdir}/genou1.gif', tick=10, dx=CHAR_W / 4, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/assis2.gif', tick=20),
             Frame(f'{subdir}/genou3.gif', tick=30, dx=CHAR_W / 4),
             Frame(f'{subdir}/assis2.gif', tick=46),
@@ -481,9 +497,9 @@ def barb(subdir: str):
         ],
         'araignee': [
             # @formatter:off
-            Frame(f'{subdir}/toile1.gif', tick=8),
+            Frame(f'{subdir}/toile1.gif', tick=8, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/toile2.gif', tick=15),
-            Frame(f'{subdir}/toile3.gif', tick=20),
+            Frame(f'{subdir}/toile3.gif', tick=20, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/toile4.gif', tick=33),
             # @formatter:on
         ],
@@ -508,7 +524,7 @@ def barb(subdir: str):
             Frame(f'{subdir}/retourne1.gif', tick=5,  mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne2.gif', tick=9,                ),  # noqa
             Frame(f'{subdir}/retourne2.gif', tick=14, mv=(CHAR_W, 0)),  # noqa
-            Frame(f'{subdir}/retourne3.gif', tick=15,               ),  # noqa
+            Frame(f'{subdir}/retourne3.gif', tick=15,                 post_action=Actions.snd('decapite.ogg')),  # noqa
             Frame(f'{subdir}/retourne3.gif', tick=19, mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne3.gif', tick=24, mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne3.gif', tick=29,               ),  # noqa
@@ -519,7 +535,7 @@ def barb(subdir: str):
             # @formatter:on
         ],
         'front': [
-            Frame(f'{subdir}/front1.gif', tick=5, dx=-1 * SCALE_X),
+            Frame(f'{subdir}/front1.gif', tick=5, dx=-1 * SCALE_X, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/front2.gif', tick=23),
             Frame(f'{subdir}/front3.gif', tick=30),
             Frame(f'{subdir}/front2.gif', tick=46),
@@ -576,7 +592,7 @@ def barb(subdir: str):
         ],
         'mort': [
             # @formatter:off
-            Frame(f'{subdir}/assis1.gif', tick=15),
+            Frame(f'{subdir}/assis1.gif', tick=15, pre_action=Actions.snd('mortKO.ogg')),
             Frame(f'{subdir}/mort2.gif',  tick=17, post_action=Actions.stop),
             Frame(f'{subdir}/mort3.gif',  tick=18, dx=-1 * CHAR_W),  # manual, see vainqueurKO
             Frame(f'{subdir}/mort4.gif',  tick=19, dx=-3 * CHAR_W),  # manual, see vainqueurKO
@@ -584,7 +600,7 @@ def barb(subdir: str):
         ],
         'mortdecap': [
             # @formatter:off
-            Frame(f'{subdir}/decap1.gif', tick=35),
+            Frame(f'{subdir}/decap1.gif', tick=35, pre_action=Actions.snd('mortdecap.ogg')),
             Frame(f'{subdir}/decap2.gif', tick=70, dx=2 * CHAR_W),
             Frame(f'{subdir}/decap3.gif', tick=80, dx=2 * CHAR_W),
             Frame(f'{subdir}/decap4.gif', tick=82, dx=2 * CHAR_W, post_action=Actions.stop),
@@ -605,6 +621,7 @@ def barb_rtl(subdir: str):
             Frame(f'{subdir}/debout.gif', xflip=True),
         ],
         'attente': [
+            Frame(f'{subdir}/attente1.gif', xflip=True, tick=8, post_action=Actions.snd('attente.ogg')),
             Frame(f'{subdir}/attente1.gif', xflip=True, tick=15),
             Frame(f'{subdir}/attente2.gif', xflip=True, tick=23, dx=-CHAR_W),
             Frame(f'{subdir}/attente3.gif', xflip=True, tick=30, dx=-CHAR_W),
@@ -646,8 +663,8 @@ def barb_rtl(subdir: str):
         ],
         'rouladeAV': [
             # @formatter:off
-            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=4,  mv=(-CHAR_W, 0)),  # noqa
-            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=7,  mv=(-CHAR_W, 0)),  # noqa
+            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=2,  mv=(-CHAR_W, 0), post_action=Actions.snd('roule.ogg')),  # noqa
+            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=4,                 ),  # noqa
             Frame(f'{subdir}/roulade1.gif', xflip=True, tick=10, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade2.gif', xflip=True, tick=13, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade3.gif',             tick=16, mv=(-CHAR_W, 0)),  # noqa
@@ -675,7 +692,7 @@ def barb_rtl(subdir: str):
         ],
         'rouladeAR': [
             # @formatter:off
-            Frame(f'{subdir}/roulade1.gif',             tick=2,                ),  # noqa
+            Frame(f'{subdir}/roulade1.gif',             tick=2,  post_action=Actions.snd('roule.ogg')),  # noqa
             Frame(f'{subdir}/roulade1.gif',             tick=5,  mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade5.gif',             tick=8,  mv=(CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade2.gif',             tick=11, mv=(CHAR_W, 0)),  # noqa
@@ -690,7 +707,8 @@ def barb_rtl(subdir: str):
         ],
         'protegeH': [
             # @formatter:off
-            Frame(f'{subdir}/marche1.gif',  xflip=True, tick=5, mv=(CHAR_W, 0)),
+            Frame(f'{subdir}/marche1.gif',  xflip=True, tick=2, mv=(CHAR_W, 0), post_action=Actions.snd('protege.ogg')),
+            Frame(f'{subdir}/marche1.gif',  xflip=True, tick=5),
             Frame(f'{subdir}/protegeH.gif', xflip=True, tick=9, dx=-CHAR_W + 2 * SCALE_X),
             # @formatter:on
         ],
@@ -700,14 +718,14 @@ def barb_rtl(subdir: str):
         ],
         'cou': [
             # @formatter:off
-            Frame(f'{subdir}/protegeH.gif', xflip=True, tick=15, dx=-CHAR_W + 2 * SCALE_X    ),  # noqa
+            Frame(f'{subdir}/protegeH.gif', xflip=True, tick=15, dx=-CHAR_W + 2 * SCALE_X, post_action=Actions.snd('epee.ogg')),  # noqa
             Frame(f'{subdir}/cou2.gif',     xflip=True, tick=30, dx=-CHAR_W + 4 * SCALE_X    ),  # noqa
             Frame(f'{subdir}/cou3.gif',     xflip=True, tick=46, dx=-4 * CHAR_W + 1 * SCALE_X),  # noqa
             # @formatter:on
         ],
         'devant': [
             # @formatter:off
-            Frame(f'{subdir}/devant1.gif', xflip=True, tick=10),
+            Frame(f'{subdir}/devant1.gif', xflip=True, tick=10, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/devant2.gif', xflip=True, tick=20),
             Frame(f'{subdir}/devant3.gif', xflip=True, tick=30, dx=-3 * CHAR_W),
             Frame(f'{subdir}/devant2.gif', xflip=True, tick=46),
@@ -715,7 +733,7 @@ def barb_rtl(subdir: str):
         ],
         'genou': [
             # @formatter:off
-            Frame(f'{subdir}/genou1.gif', xflip=True, tick=10, dx=-CHAR_W / 4),
+            Frame(f'{subdir}/genou1.gif', xflip=True, tick=10, dx=-CHAR_W / 4, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/assis2.gif', xflip=True, tick=20),
             Frame(f'{subdir}/genou3.gif', xflip=True, tick=30, dx=-CHAR_W / 4
                                                                   - 3 * CHAR_W),
@@ -724,9 +742,9 @@ def barb_rtl(subdir: str):
         ],
         'araignee': [
             # @formatter:off
-            Frame(f'{subdir}/toile1.gif', xflip=True, tick=8,  dx=-CHAR_W),
+            Frame(f'{subdir}/toile1.gif', xflip=True, tick=8,  dx=-CHAR_W, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/toile2.gif', xflip=True, tick=15, dx=-CHAR_W),
-            Frame(f'{subdir}/toile3.gif', xflip=True, tick=20, dx=-CHAR_W),
+            Frame(f'{subdir}/toile3.gif', xflip=True, tick=20, dx=-CHAR_W, post_action=Actions.snd('epee.ogg')),
             Frame(f'{subdir}/toile4.gif', xflip=True, tick=33, dx=-3 * CHAR_W),
             # @formatter:on
         ],
@@ -751,7 +769,7 @@ def barb_rtl(subdir: str):
             Frame(f'{subdir}/retourne1.gif', xflip=True, tick=5,  mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne2.gif', xflip=True, tick=9,                 ),  # noqa
             Frame(f'{subdir}/retourne2.gif', xflip=True, tick=14, mv=(-CHAR_W, 0)),  # noqa
-            Frame(f'{subdir}/retourne3.gif', xflip=True, tick=15,                ),  # noqa
+            Frame(f'{subdir}/retourne3.gif', xflip=True, tick=15,                  post_action=Actions.snd('decapite.ogg')),  # noqa
             Frame(f'{subdir}/retourne3.gif', xflip=True, tick=19, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne3.gif', xflip=True, tick=24, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/retourne3.gif', xflip=True, tick=29,                ),  # noqa
@@ -763,7 +781,7 @@ def barb_rtl(subdir: str):
         ],
         'front': [
             # @formatter:off
-            Frame(f'{subdir}/front1.gif', xflip=True, tick=5,  dx=-CHAR_W + 1 * SCALE_X),  # noqa
+            Frame(f'{subdir}/front1.gif', xflip=True, tick=5,  dx=-CHAR_W + 1 * SCALE_X, post_action=Actions.snd('epee.ogg')),  # noqa
             Frame(f'{subdir}/front2.gif', xflip=True, tick=23, dx=-CHAR_W              ),  # noqa
             Frame(f'{subdir}/front3.gif', xflip=True, tick=30, dx=-3 * CHAR_W          ),  # noqa
             Frame(f'{subdir}/front2.gif', xflip=True, tick=46, dx=-CHAR_W              ),  # noqa
@@ -823,7 +841,7 @@ def barb_rtl(subdir: str):
         ],
         'mort': [
             # @formatter:off
-            Frame(f'{subdir}/assis1.gif', xflip=True, tick=15),
+            Frame(f'{subdir}/assis1.gif', xflip=True, tick=15, pre_action=Actions.snd('mortKO.ogg')),
             Frame(f'{subdir}/mort2.gif',  xflip=True, tick=17, dx=-1 * CHAR_W, post_action=Actions.stop),
             Frame(f'{subdir}/mort3.gif',  xflip=True, tick=18),  # manual, see vainqueurKO
             Frame(f'{subdir}/mort4.gif',  xflip=True, tick=19),  # manual, see vainqueurKO
@@ -831,7 +849,7 @@ def barb_rtl(subdir: str):
         ],
         'mortdecap': [
             # @formatter:off
-            Frame(f'{subdir}/decap1.gif', xflip=True, tick=35, dx=-1 * CHAR_W),
+            Frame(f'{subdir}/decap1.gif', xflip=True, tick=35, dx=-1 * CHAR_W, pre_action=Actions.snd('mortdecap.ogg')),
             Frame(f'{subdir}/decap2.gif', xflip=True, tick=70, dx=-3 * CHAR_W),
             Frame(f'{subdir}/decap3.gif', xflip=True, tick=80, dx=-4 * CHAR_W),
             Frame(f'{subdir}/decap4.gif', xflip=True, tick=82, dx=-5 * CHAR_W, post_action=Actions.stop),
@@ -946,9 +964,9 @@ def sorcier():
             Frame('sprites/drax1.gif'),
         ],
         'attaque': [
-            Frame('sprites/drax1.gif', tick=50),
+            Frame('sprites/drax1.gif', tick=50, post_action=Actions.snd('feu.ogg')),
             Frame('sprites/drax2.gif', tick=60),
-            Frame('sprites/drax1.gif', tick=130),
+            Frame('sprites/drax1.gif', tick=130, post_action=Actions.snd('feu.ogg')),
             Frame('sprites/drax2.gif', tick=140),
             Frame('sprites/drax1.gif', tick=141, post_action=Actions.stop),
         ],

barbariantuw/sprites.py
@@ -877,7 +877,6 @@ class Barbarian(AnimatedSprite):
             self.state = State.debout
         elif temps == self.reftemps + 8:
             self.animate('attente', 8)
-            self.snd_play('attente.ogg')
 
     def gestion_avance(self, temps, opponent: 'Barbarian',
                        soncling: iter, songrogne: iter):
@@ -1033,7 +1032,6 @@ class Barbarian(AnimatedSprite):
 
         elif temps == self.reftemps + 2:
             self.xM = self.x_loc() + (0 if self.rtl else 4)
-            self.snd_play('roule.ogg')
             self.animate('rouladeAV', 2)
 
     def gestion_rouladeAR(self, temps):
@@ -1047,7 +1045,6 @@ class Barbarian(AnimatedSprite):
             self.occupe = False
             self.state = State.debout
         elif temps == self.reftemps + 2:
-            self.snd_play('roule.ogg')
             self.animate('rouladeAR', 2)
 
     def gestion_protegeH1(self, temps):
@@ -1059,7 +1056,6 @@ class Barbarian(AnimatedSprite):
             self.state = State.protegeH
             self.occupe = False
         elif temps == self.reftemps + 2:
-            self.snd_play('protege.ogg')
             self.animate('protegeH', 2)
 
     def gestion_protegeH(self, temps, opponent: 'Barbarian',
@@ -1067,7 +1063,7 @@ class Barbarian(AnimatedSprite):
         self.reset_xX_front()
         self.xAtt = self.x_loc() + (4 if self.rtl else 0)
         self.yG = YG
-        self.set_anim_frame('protegeH', 1)
+        self.set_anim_frame('protegeH', 2)
         if self.attaque:
             self.occupe_state(State.araignee, temps)
             self.gestion_araignee(temps, opponent, soncling, songrogne)
@@ -1124,7 +1120,6 @@ class Barbarian(AnimatedSprite):
                 self.xAtt = self.x_loc() + (-3 if self.rtl else 7)
 
         elif temps == self.reftemps + 16:
-            self.snd_play('epee.ogg')
             self.yAtt = self.yT
 
         elif temps == self.reftemps + 4:
@@ -1156,7 +1151,6 @@ class Barbarian(AnimatedSprite):
 
         elif temps == self.reftemps + 11:
             self.snd_play(next(songrogne))
-            self.snd_play('epee.ogg')
             self.yAtt = self.yM
 
         elif temps == self.reftemps:
@@ -1185,8 +1179,8 @@ class Barbarian(AnimatedSprite):
                 self.xAtt = self.x_loc() + (-3 if self.rtl else 7)
         elif temps == self.reftemps + 11:
             self.snd_play(next(songrogne))
-            self.snd_play('epee.ogg')
             self.yAtt = self.yG
+
         elif temps == self.reftemps:
             self.animate('genou')
 
@@ -1204,7 +1198,6 @@ class Barbarian(AnimatedSprite):
             self.state = State.debout
 
         elif temps == self.reftemps + 21:
-            self.snd_play('epee.ogg')
             if opponent.state == State.araignee:
                 distance = abs(self.x_loc() - opponent.x_loc())
                 # cycle and play cling-sound once (for one player only)
@@ -1215,7 +1208,6 @@ class Barbarian(AnimatedSprite):
 
         elif temps == self.reftemps + 8:
             self.snd_play(next(songrogne))
-            self.snd_play('epee.ogg')
 
         elif temps == self.reftemps:
             self.animate('araignee')
@@ -1279,8 +1271,6 @@ class Barbarian(AnimatedSprite):
         elif temps > self.reftemps + 50:
             self.yAtt = YT
             self.xAtt = self.x_loc() + (-3 if self.rtl else 7)
-        elif temps == self.reftemps + 15:
-            self.snd_play('decapite.ogg')
         elif temps == self.reftemps + 2:
             self.animate('decapite', 2)
 
@@ -1307,7 +1297,6 @@ class Barbarian(AnimatedSprite):
 
         elif temps == self.reftemps + 6:
             self.snd_play(next(songrogne))
-            self.snd_play('epee.ogg')
             self.yAtt = self.yF
 
         elif temps == self.reftemps + 4:
@@ -1392,14 +1381,12 @@ class Barbarian(AnimatedSprite):
         self.on_mort(self)
         self.animate('mort')
         opponent.occupe_state(State.vainqueurKO, temps)
-        self.snd_play('mortKO.ogg')
 
     def gestion_mortedecap(self, temps, opponent: 'Barbarian'):
         if temps == self.reftemps:
             self.on_mort(self)
             self.animate('mortdecap')
             opponent.occupe_state(State.vainqueur, temps)
-            self.snd_play('mortdecap.ogg')
 
     def gestion_vainqueur(self):
         self.xAtt = self.x_loc()
@@ -1620,7 +1607,6 @@ class Sorcier(AnimatedSprite):
 
         elif temps == self.reftemps + 131:
             self.yAtt = YT
-            self.snd_play('feu.ogg')
             # noinspection PyTypeChecker
             self.feu.add(self.groups())
             self.feu.top_left = loc(self.xAtt, self.yAtt)
@@ -1634,7 +1620,6 @@ class Sorcier(AnimatedSprite):
             self.yAtt = YG
 
         elif temps == self.reftemps + 51:
-            self.snd_play('feu.ogg')
             # noinspection PyTypeChecker
             self.feu.add(self.groups())
             self.feu.top_left = loc(self.xAtt, self.yAtt)

commit 35b6397fea4be865d50799f7090d97d7a4455216

Fix: JoueurB RouladeAV frames (broken in 396bb036)

barbariantuw/anims.py
@@ -682,6 +682,7 @@ def barb_rtl(subdir: str):
             # @formatter:off
             Frame(f'{subdir}/roulade1.gif', xflip=True, tick=2,  mv=(-CHAR_W, 0), post_action=Actions.snd('roule.ogg')),  # noqa
             Frame(f'{subdir}/roulade1.gif', xflip=True, tick=4,                 ),  # noqa
+            Frame(f'{subdir}/roulade1.gif', xflip=True, tick=7,  mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade1.gif', xflip=True, tick=10, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade2.gif', xflip=True, tick=13, mv=(-CHAR_W, 0)),  # noqa
             Frame(f'{subdir}/roulade3.gif',             tick=16, mv=(-CHAR_W, 0)),  # noqa

barbariantuw/sprites.py
@@ -1038,9 +1038,7 @@ class Barbarian(AnimatedSprite):
         elif temps == self.reftemps + 14:
             self.xAtt = self.x_loc() + (-1 if self.rtl else 5)
 
-        elif temps > self.reftemps + 10:
-            pass  # do not update xM after reftemps+10
-        elif temps > self.reftemps + 2:
+        elif 2 < temps - self.reftemps < 11:
             self.xM = self.x_loc() + (0 if self.rtl else 4)
 
         elif temps == self.reftemps + 2: